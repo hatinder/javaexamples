cc7e1c5dc7f726b88d0111480d287daa3a056e22
hatinder <hatinder.jeet@gmail.com>
2021-06-19 15:56:05 +0100

    HelloWorld

---
+++ springboot-helloworld-cli/src/main/java/com/examples/utility/LogWrapper.java
@@ -0,0 +1,44 @@
+package com.examples.utility;
+
+import org.slf4j.Logger;
+
+public class LogWrapper
+{
+    private Logger logger;
+
+
+    public LogWrapper(Logger logger)
+    {
+        this.logger = logger;
+    }
+
+
+    public void debugFormat(String format, Object... args)
+    {
+        if (logger.isDebugEnabled())
+        {
+            logger.debug(String.format(format, args));
+        }
+    }
+
+    public void errorFormat(String format, Object... args)
+    {
+        if (logger.isErrorEnabled())
+        {
+            logger.error(String.format(format, args));
+        }
+    }
+
+    public void infoFormat(String format, Object... args)
+    {
+        if (logger.isInfoEnabled())
+        {
+            logger.info(String.format(format, args));
+        }
+    }
+
+    public Logger getLogger(){
+        return this.logger;
+    }
+
+}
